dd8c4bcb82310ccecda3711bd92f16be8842030b
lkoujalagi <laxman.koujalagi@elastica.co>
2017-12-15 01:33:14 +0530

    first commit

---
+++ src/test/java/com/crm/qa/testcases/LoginPageTest.java
@@ -0,0 +1,50 @@
+package com.crm.qa.testcases;
+
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import com.crm.qa.base.TestBase;
+import com.crm.qa.pages.HomePage;
+import com.crm.qa.pages.LoginPage;
+
+public class LoginPageTest extends TestBase {
+	
+	LoginPage loginPage;
+	HomePage homePage;
+	
+	public LoginPageTest(){
+		super();
+	}
+	
+	@BeforeMethod
+	public void setUp(){
+		initialization();
+		loginPage = new LoginPage();
+	}
+	
+	@Test
+	public void loginPageTitleTest(){
+		String title = loginPage.validateLoginPageTitel();
+		Assert.assertEquals(title,"#1 Free CRM for Any Business: Online Customer Relationship Software");
+	}
+	
+	@Test
+	public void crmLogoImageTest(){
+		boolean flag=loginPage.validateCRMImage();
+		Assert.assertTrue(flag);
+	}
+	
+	@Test
+	public void LoginTest(){
+		homePage=loginPage.login(prop.getProperty("username"), prop.getProperty("password"));
+	}
+	
+	@AfterMethod
+	public void tearDown()
+	{
+		driver.quit();
+	}
+
+}
